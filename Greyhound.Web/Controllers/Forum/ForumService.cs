8b9515788e2b4e6e50cae8fab843988f8697dae7
ieamarathunga <ieamarathunga@hotmail.com>
2016-08-17 08:55:17 +0530

    Forum Model Creation - Initial Checking

---
+++ Greyhound.Web/Controllers/Forum/ForumService.cs
@@ -0,0 +1,23 @@
+﻿using GreyHound.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Greyhound.Web.Controllers.Forum
+{
+    public class ForumService : IForumService
+    {
+        private readonly GreyHoundContext _dbContext;
+
+        public ForumService()
+        {
+            _dbContext = new GreyHoundContext();
+        }
+
+        public ForumService(GreyHoundContext context)
+        {
+            _dbContext = context;
+        }
+    }
+}
